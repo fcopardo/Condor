3cbd0797339441f35c8ea07e55885c3b185f7b4b
Miguel A. Zapata <miguelo.zapata@gmail.com>
2015-01-25 21:59:39 -0300

    Add code to Login Twitter Add layout WebView

--- app/src/main/java/bit01/condor/Activities/LoginTwitter.java
+++ app/src/main/java/bit01/condor/Activities/LoginTwitter.java
@@ -4,60 +4,172 @@ package bit01.condor.Activities;
  */
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.io.InputStream;
 
 import bit01.condor.R;
+import bit01.condor.Utils.LocalStorage;
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.User;
+import twitter4j.auth.AccessToken;
 import twitter4j.auth.RequestToken;
-
 
 public class LoginTwitter extends Activity {
 
-    static final String PREF_KEY_OAUTH_TOKEN = "oauth_token";
-    static final String PREF_KEY_OAUTH_SECRET = "oauth_token_secret";
-    static final String TWITTER_CALLBACK_URL = "oauth://bit01.bitbucket.org";
-    // / Twitter oauth urls
-    static final String URL_TWITTER_AUTH = "auth_url";
-    static final String URL_TWITTER_OAUTH_VERIFIER = "oauth_verifier";
-    static final String URL_TWITTER_OAUTH_TOKEN = "oauth_token";
-    /**
-     * Register your here app https://dev.twitter.com/apps/new and get your
-     * consumer key and secret
-     * To load this, put in raw folder a file called "twitter_keys.txt" and write the
-     * keys in this format:
-     * CONSUMER_KEY:CONSUMER_SECRET
-     */
-    static String TWITTER_CONSUMER_KEY = "";
-    static String TWITTER_CONSUMER_SECRET = "";
-    private static Twitter twitter;
-    private static RequestToken requestToken;
+    Twitter twitter;
+    RequestToken requestToken = null;
+    String oauth_url;
+    String oauth_verifier;
+    AccessToken accessToken;
+
+    private Dialog auth_dialog;
+    private ProgressDialog progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        LocalStorage.initLocalStorage(this);
         readTwitterKeys();
+
+        twitter = new TwitterFactory().getInstance();
+        twitter.setOAuthConsumer(LocalStorage.getConsumerKey(), LocalStorage.getConsumerSecret());
+
+        Button buttonLoginTwitter = (Button) findViewById(R.id.button_login_twitter);
+        buttonLoginTwitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginProcess();
+            }
+        });
+
+    }
+
+    private void LoginProcess() {
+        new TokenGet().execute();
     }
 
     private void readTwitterKeys() {
         try {
             Resources res = getResources();
             InputStream inputStream = res.openRawResource(R.raw.twitter_keys);
-
             byte[] b = new byte[inputStream.available()];
             inputStream.read(b);
             String[] twitterKeys = new String(b).split(":");
-            TWITTER_CONSUMER_KEY = twitterKeys[0];
-            TWITTER_CONSUMER_SECRET = twitterKeys[1];
-            Log.d("Twitter Key", "Consumer Key: " + TWITTER_CONSUMER_KEY);
-            Log.d("Twitter Key", "Consumer Secret: " + TWITTER_CONSUMER_SECRET);
+
+            LocalStorage.setConsumerKey(twitterKeys[0]);
+            LocalStorage.setConsumerSecret(twitterKeys[1]);
+
+            Log.d("Twitter Key", "Consumer Key: " + LocalStorage.getConsumerKey());
+            Log.d("Twitter Key", "Consumer Secret: " + LocalStorage.getConsumerSecret());
         } catch (Exception e) {
             e.printStackTrace();
             Log.d("LoginTwitter Error", "Cant get Twitter Keys");
+        }
+    }
+
+    private class TokenGet extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                requestToken = twitter.getOAuthRequestToken();
+                oauth_url = requestToken.getAuthorizationURL();
+            } catch (TwitterException e) {
+                e.printStackTrace();
+            }
+            return oauth_url;
+        }
+
+        @Override
+        protected void onPostExecute(String oauth_url) {
+            if (oauth_url != null) {
+                Log.d("URL Twitter", oauth_url);
+                auth_dialog = new Dialog(LoginTwitter.this);
+                auth_dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                auth_dialog.setContentView(R.layout.login_webview);
+                WebView web = (WebView) auth_dialog.findViewById(R.id.webview_LoginTwitter);
+                web.loadUrl(oauth_url);
+                web.setWebViewClient(new WebViewClient() {
+                    boolean authComplete = false;
+
+                    @Override
+                    public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                        super.onPageStarted(view, url, favicon);
+                    }
+
+                    @Override
+                    public void onPageFinished(WebView view, String url) {
+                        super.onPageFinished(view, url);
+                        if (url.contains("oauth_verifier") && !authComplete) {
+                            authComplete = true;
+                            Log.e("Url", url);
+                            Uri uri = Uri.parse(url);
+                            oauth_verifier = uri.getQueryParameter("oauth_verifier");
+                            auth_dialog.dismiss();
+                            new AccessTokenGet().execute();
+                        } else if (url.contains("denied")) {
+                            auth_dialog.dismiss();
+                            Toast.makeText(LoginTwitter.this, getString(R.string.access_denied), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                auth_dialog.show();
+                auth_dialog.setCancelable(true);
+            } else {
+                Toast.makeText(LoginTwitter.this, getString(R.string.invalid_credentials), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private class AccessTokenGet extends AsyncTask<String, String, Boolean> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progress = new ProgressDialog(LoginTwitter.this);
+            progress.setMessage(getString(R.string.fetch_data));
+            progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            progress.setIndeterminate(true);
+            progress.show();
+        }
+
+        @Override
+        protected Boolean doInBackground(String... args) {
+            try {
+                accessToken = twitter.getOAuthAccessToken(requestToken, oauth_verifier);
+                LocalStorage.setOauthToken(accessToken.getToken());
+                LocalStorage.setOauthSecret(accessToken.getTokenSecret());
+
+                User user = twitter.showUser(accessToken.getUserId());
+                Log.d("Condor", "Twitter user: " + user.getName() + " logued sucess");
+
+            } catch (TwitterException e) {
+                e.printStackTrace();
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean response) {
+            if (response) {
+                progress.hide();
+            }
         }
     }
 }
--- app/src/main/java/bit01/condor/Utils/LocalStorage.java
+++ app/src/main/java/bit01/condor/Utils/LocalStorage.java
@@ -10,10 +10,13 @@ import android.content.SharedPreferences;
 
 public class LocalStorage {
     private static final String _SHARED_PREFERENCE_FILE = "cl.bit01.condor.local_storage";
+    private static final String KEY_USER_REGISTERED = "user_loggin";
+    private static final String TWITTER_CONSUMER_KEY = "consumer_key";
+    private static final String TWITTER_CONSUMER_SECRET = "consumer_secret";
+    private static final String KEY_OAUTH_TOKEN = "oauth_token";
+    private static final String KEY_OAUTH_SECRET = "oauth_token_secret";
     private static SharedPreferences _SHARED_PREFERENCE;
     private static SharedPreferences.Editor _PREFERENCE_EDITOR;
-
-    private static String KEY_USER_REGISTERED = "user_loggin";
 
     public static void initLocalStorage(Context context) {
         _SHARED_PREFERENCE = context.getSharedPreferences(_SHARED_PREFERENCE_FILE, Activity.MODE_PRIVATE);
@@ -26,6 +29,46 @@ public class LocalStorage {
     public static void setLoggedInTwitter() {
         _PREFERENCE_EDITOR = _SHARED_PREFERENCE.edit();
         _PREFERENCE_EDITOR.putBoolean(KEY_USER_REGISTERED, true);
+        _PREFERENCE_EDITOR.apply();
+    }
+
+    public static String getConsumerKey() {
+        return _SHARED_PREFERENCE.getString(TWITTER_CONSUMER_KEY, "");
+    }
+
+    public static void setConsumerKey(String consumerKey) {
+        _PREFERENCE_EDITOR = _SHARED_PREFERENCE.edit();
+        _PREFERENCE_EDITOR.putString(TWITTER_CONSUMER_KEY, consumerKey);
+        _PREFERENCE_EDITOR.apply();
+    }
+
+    public static String getConsumerSecret() {
+        return _SHARED_PREFERENCE.getString(TWITTER_CONSUMER_SECRET, "");
+    }
+
+    public static void setConsumerSecret(String consumerSecret) {
+        _PREFERENCE_EDITOR = _SHARED_PREFERENCE.edit();
+        _PREFERENCE_EDITOR.putString(TWITTER_CONSUMER_SECRET, consumerSecret);
+        _PREFERENCE_EDITOR.apply();
+    }
+
+    public static String getOauthToken() {
+        return _SHARED_PREFERENCE.getString(KEY_OAUTH_TOKEN, "");
+    }
+
+    public static void setOauthToken(String oauthToken) {
+        _PREFERENCE_EDITOR = _SHARED_PREFERENCE.edit();
+        _PREFERENCE_EDITOR.putString(KEY_OAUTH_TOKEN, oauthToken);
+        _PREFERENCE_EDITOR.apply();
+    }
+
+    public static String getOauthSecret() {
+        return _SHARED_PREFERENCE.getString(KEY_OAUTH_SECRET, "");
+    }
+
+    public static void setOauthSecret(String oauthKey) {
+        _PREFERENCE_EDITOR = _SHARED_PREFERENCE.edit();
+        _PREFERENCE_EDITOR.putString(KEY_OAUTH_SECRET, oauthKey);
         _PREFERENCE_EDITOR.apply();
     }
 }

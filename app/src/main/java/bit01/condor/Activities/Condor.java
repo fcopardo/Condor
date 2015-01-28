7f4bff27c72745d7b43bbe906615c56bf8d7de5a
Miguel A. Zapata <miguelo.zapata@gmail.com>
2015-01-27 22:16:18 -0300

    Added Navigation Drawer

--- app/src/main/java/bit01/condor/Activities/Condor.java
+++ app/src/main/java/bit01/condor/Activities/Condor.java
@@ -3,16 +3,57 @@ package bit01.condor.Activities;
  * Created by miguelost on 25-01-15.
  */
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import bit01.condor.R;
+import bit01.condor.Utils.NavBarAdapter;
 
-public class Condor extends Activity {
+public class Condor extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_condor);
+
+        String twitterName = "Miguel A. Zapata";
+        String twitterUser = "@Miguelost";            //or other secondary text, just Email
+        String twitterAvatarUrl = "";
+
+        String[] mDrawerTitles = getResources().getStringArray(R.array.navigation_drawer_items);
+        int mDrawerIcons[] = new int[1];
+        mDrawerIcons[0] = (R.drawable.ic_reorder);      //Icon For Timeline
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.RecyclerViewDrawer);
+        RecyclerView.Adapter mAdapter = new NavBarAdapter(mDrawerTitles, mDrawerIcons, twitterName, twitterUser, twitterAvatarUrl);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.DrawerLayout);
+        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar,
+                R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
     }
 }
---
+++ app/src/main/java/bit01/condor/Utils/NavBarAdapter.java
@@ -0,0 +1,100 @@
+package bit01.condor.Utils;
+/*
+ * Created by miguelost on 27-01-15.
+ */
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import bit01.condor.R;
+
+public class NavBarAdapter extends RecyclerView.Adapter<NavBarAdapter.ViewHolder> {
+
+    private static final int TYPE_HEADER = 0;
+    private static final int TYPE_ITEM = 1;
+
+    private String mNavTitles[];
+    private int mIcons[];
+
+    private String nameTwitter;
+    private String userTwitter;
+    private String avatarTwitterUrl;
+
+    public NavBarAdapter(String Titles[], int Icons[], String nameTwitter, String userTwitter, String avatarUrl) {
+        this.mNavTitles = Titles;
+        this.mIcons = Icons;
+        this.nameTwitter = nameTwitter;
+        this.userTwitter = userTwitter;
+        this.avatarTwitterUrl = avatarUrl;
+    }
+
+    @Override
+    public NavBarAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        if (viewType == TYPE_ITEM) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row, parent, false);
+            return new ViewHolder(v, viewType);
+        } else if (viewType == TYPE_HEADER) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.header, parent, false);
+            return new ViewHolder(v, viewType);
+        }
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(NavBarAdapter.ViewHolder holder, int position) {
+        if (holder.Holderid == 1) {
+            holder.textView.setText(mNavTitles[position - 1]);
+            holder.imageView.setImageResource(mIcons[position - 1]);
+        } else {
+            //holder.twitterAvatar.setImageResource(avatarTwitterUrl); replace for picasso
+            holder.twitterName.setText(nameTwitter);
+            holder.twitterUser.setText(userTwitter);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNavTitles.length + 1;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (isPositionHeader(position))
+            return TYPE_HEADER;
+
+        return TYPE_ITEM;
+    }
+
+    private boolean isPositionHeader(int position) {
+        return position == 0;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        int Holderid;
+        TextView textView;
+        ImageView imageView;
+        ImageView twitterAvatar;
+        TextView twitterName;
+        TextView twitterUser;
+
+        public ViewHolder(View itemView, int ViewType) {
+            super(itemView);
+
+            if (ViewType == TYPE_ITEM) {
+                textView = (TextView) itemView.findViewById(R.id.rowText);
+                imageView = (ImageView) itemView.findViewById(R.id.rowIcon);
+                Holderid = 1;
+            } else {
+                twitterName = (TextView) itemView.findViewById(R.id.twitterName);
+                twitterUser = (TextView) itemView.findViewById(R.id.twitterUser);
+                twitterAvatar = (ImageView) itemView.findViewById(R.id.userImageView);
+                Holderid = 0;
+            }
+        }
+    }
+}
